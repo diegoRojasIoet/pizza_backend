735bd7f32127659ec7eb4cd49442615cae14e25a
diegoRojasIoet <diego.rojas@ioet.com>
2023-05-19 17:05:20 -0500

    feat/seeder-for-db

--- app/plugins.py
+++ app/plugins.py
@@ -2,6 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_marshmallow import Marshmallow
 
+
+
 convention = {
     "ix": 'ix_%(column_0_label)s',
     "uq": "uq_%(table_name)s_%(column_0_name)s",
